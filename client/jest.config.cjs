28b4b654483ed449fb2af952a520e3bcd44b7ed7
EfrataAron <ephretaselasie@gmail.com>
2025-09-17 16:37:08 +0300

    jest, npm test

---
+++ client/jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-react'] }],
+  },
+};
+
---
+++ client/src/__tests__/smoke.test.js
@@ -0,0 +1,12 @@
+import reducer from '../Reducers';
+
+test('reducer initializes with empty searchData', () => {
+  const state = reducer(undefined, { type: '@@INIT' });
+  expect(state.searchData).toEqual([]);
+});
+
+test('reducer handles CHANGE_SEARCH_DATA', () => {
+  const next = reducer(undefined, { type: 'CHANGE_SEARCH_DATA', data: [{ first_name: 'Ada' }] });
+  expect(next.searchData).toEqual([{ first_name: 'Ada' }]);
+});
+
